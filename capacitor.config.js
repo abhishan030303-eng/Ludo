2240353b5221effe81386cb9471e050e50da380e
abhishan030303-eng <abhishan030303@gmail.com>
2026-08-13 01:12:18 +0530

    Delete capacitor.config.js

--- capacitor.config.js
+++
@@ -1,8 +0,0 @@
-const { CapacitorConfig } = require("@capacitor/cli");
-const config = {
-  appId: "com.abhiludo.offline",
-  appName: "ABHI LUDO",
-  webDir: "www",
-  bundledWebRuntime: false
-};
-module.exports = config;
